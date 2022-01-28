da654411158df402b6d5af9a1d19a38ed3579bea
sky6min <sky6min@gmail.com>
2022-01-28 13:50:02 +0900

    .gitignore dist

---
+++ frontend/dist/_nuxt/1125059.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[151],{682:function(n,e,t){"use strict";t.r(e);var l={components:{Landing:t(667).default}},c=t(55),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("Landing")}),[],!1,null,null,null);e.default=component.exports}}]);
