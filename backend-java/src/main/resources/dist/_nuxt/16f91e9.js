105f5bbe2e1a073e881d058892eaf9ed455340a9
yeongki0944 <87894636+yeongki0944@users.noreply.github.com>
2022-02-08 14:03:31 +0900

    fix tensor.js model loading
    fix tensor.js model loading

---
+++ backend-java/src/main/resources/dist/_nuxt/16f91e9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[62],{577:function(t,e,o){"use strict";o.r(e);var n=o(399),c=o.n(n),r=localStorage.getItem("jwtToken")||"";e.default=c.a.create({baseURL:"http://localhost:8080",headers:{"Content-type":"application/json",Authorization:r},withCredentials:!0})}}]);
---
+++ backend-java/src/main/resources/dist/_nuxt/ccead7f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[166],{1082:function(n,e,t){"use strict";t.r(e);var l={components:{Landing:t(1066).default}},c=t(57),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("Landing")}),[],!1,null,null,null);e.default=component.exports}}]);
