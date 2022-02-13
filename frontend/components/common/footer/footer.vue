2e1639812a5c5a2250d9f4d44f79b29618eeb001
손한기 <wwenxt@naver.com>
2022-02-13 22:00:22 +0900

    footer fix

--- frontend/components/common/footer/footer.vue
+++ frontend/components/common/footer/footer.vue
@@ -39,10 +39,10 @@
               <p class="text-black align-items-center">
                 손한기
                 <a class="icon-btn btn-google button-effect btn-sm" href="onegison95@gmail.com">
-                  <i class="fa fa-google"></i>
+                  <i class="fa-brands fa-google"></i>
                 </a>
                 <a class="icon-btn btn-github button-effect btn-sm" href="https://www.github.com/onegi95/">
-                  <i class="fa fa-github"></i>
+                  <i class="fa-brands fa-github"></i>
                 </a>
               </p>
             </div>
