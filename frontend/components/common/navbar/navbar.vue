99511f33b8729fb5e1be16b4ec7878ba891431b1
yeongki0944 <87894636+yeongki0944@users.noreply.github.com>
2022-02-16 14:14:51 +0900

    fix navbar
    fix navbar

--- frontend/components/common/navbar/navbar.vue
+++ frontend/components/common/navbar/navbar.vue
@@ -3,7 +3,7 @@
   <div class="header-right-margin">
     <client-only>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <b-navbar-nav justify-content-between>
           <button type="button" class="btn btn-primary">
             <nuxt-link to="/online/room">온라인 진료실</nuxt-link>
           </button>
@@ -63,7 +63,7 @@
             >
           </b-dropdown>
 
-          <button v-if="!isLoginGetters" type="button" class="btn btn-primary">
+          <!-- <button v-if="!isLoginGetters" type="button" class="btn btn-primary">
             <nuxt-link to="/authentication/login">로그인</nuxt-link>
           </button>
           <button v-if="!isLoginGetters" type="button" class="btn btn-primary">
@@ -72,109 +72,160 @@
           <button v-if="isLoginGetters" type="button" class="btn btn-primary">
             <nuxt-link to="/authentication/mypage">마이페이지</nuxt-link>
           </button>
-          <!-- 로그인과 회원가입만 사용함 -->
-          <!-- <b-dropdown id="dropdown-3" text="Authentication">
 
-                    </b-dropdown>
-                    <button v-if="! isLoginGetters" type="button" class="btn btn-primary">
-                        <nuxt-link to="/authentication/login">로그인</nuxt-link>
-                    </button>
-                    <button v-if="! isLoginGetters" type="button" class="btn btn-primary">
-                        <nuxt-link to="/authentication/signup-2"
-                            >회원가입</nuxt-link
-                        >
-                    </button>
+          <button
+            v-if="isLoginGetters"
+            id="logout"
+            type="button"
+            class="btn btn-primary"
+          >
+            <a @click="logout">로그아웃</a>
+          </button>
 
+          <button
+            v-if="isManagerGetters"
+            id="mystate"
+            type="button"
+            class="btn btn-primary"
+          >
+            <nuxt-link to="/manager/manager">관리자페이지</nuxt-link>
+          </button>
+          <button
+            v-if="isLoginGetters && !isManagerGetters && !isDoctorGetters"
+            id="mystate"
+            type="button"
+            class="btn btn-primary"
+          >
+            <nuxt-link to="/reserve/applyReservation">예약신청</nuxt-link>
+          </button>
+          <button
+            v-if="isLoginGetters && !isManagerGetters"
+            id="mystate"
+            type="button"
+            class="btn btn-primary"
+          >
+            <nuxt-link to="/reserve/checkReservation">예약확인</nuxt-link>
+          </button>
 
-                        <b-dropdown-item class="nav-link"
-                            ><nuxt-link to="/authentication/login"
-                                >로그인</nuxt-link
-                            ></b-dropdown-item
-                        >
-                        <b-dropdown-item class="nav-link"
-                            ><nuxt-link to="/authentication/login-2"
-                                >Signin Classic</nuxt-link
-                            ></b-dropdown-item
-                        >
-                        <b-dropdown-item class="nav-link"
-                            ><nuxt-link to="/authentication/signup"
-                                >회원가입</nuxt-link
-                            ></b-dropdown-item
-                        >
-                        <b-dropdown-item class="nav-link"
-                            ><nuxt-link to="/authentication/signup-2"
-                                >Signup Classic</nuxt-link
-                            ></b-dropdown-item
-                        >
-                    </b-dropdown> -->
-
-
-                    <button v-if="isLoginGetters" id="logout" type="button" class="btn btn-primary">
-                    <a @click="logout"
-                        >로그아웃</a
-                        >
-                    </button>
-
-                    <button v-if="isManagerGetters" id="mystate" type="button" class="btn btn-primary">
-                        <nuxt-link to="/manager/manager"
-                            >관리자페이지</nuxt-link
-                        >
-                    </button>
-                    <button  v-if="isLoginGetters && ! isManagerGetters && ! isDoctorGetters " id="mystate" type="button" class="btn btn-primary">
-                        <nuxt-link to="/reserve/applyReservation"
-                            >예약신청</nuxt-link
-                        >
-                    </button>
-                    <button v-if="isLoginGetters && ! isManagerGetters" id="mystate" type="button" class="btn btn-primary">
-                        <nuxt-link to="/reserve/checkReservation"
-                            >예약확인</nuxt-link
-                        >
-                    </button>  
-
-                    <button v-if="isLoginGetters" id="mystate" type="button" class="btn btn-primary">
-                    <h5 style="float:left"
-                        >안녕하세요! {{this.$store.state.login.login.userName}} <span style="font-size:14px" v-if="isDoctorGetters">의사</span>님                    
-                        <img
-                        v-if="isLoginGetters"
-                        src="@/assets/images/noProfile.png"
-                        alt=""
-                        width="24"
-                        height="20"
-                        style="border-radius: 50%; margin-left:10px; padding-bottom:3px"
-                        class="d-inline-block align-text-top"
-                    />  </h5
-                        >
-                    </button>
-
+          <button
+            v-if="isLoginGetters"
+            id="mystate"
+            type="button"
+            class="btn btn-primary"
+          >
+            <h5 style="float: left">
+              안녕하세요! {{ this.$store.state.login.login.userName }}
+              <span style="font-size: 14px" v-if="isDoctorGetters">의사</span>님
+              <img
+                v-if="isLoginGetters"
+                src="@/assets/images/noProfile.png"
+                alt=""
+                width="24"
+                height="20"
+                style="
+                  border-radius: 50%;
+                  margin-left: 10px;
+                  padding-bottom: 3px;
+                "
+                class="d-inline-block align-text-top"
+              />
+            </h5>
+          </button> -->
         </b-navbar-nav>
+        <b-navbar-nav id="right-nav">
+          <b-nav-item v-if="!isLoginGetters">
+            <button type="button" class="btn btn-primary">
+              <nuxt-link to="/authentication/login">로그인</nuxt-link>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="!isLoginGetters">
+            <button type="button" class="btn btn-primary">
+              <nuxt-link to="/authentication/signup-2">회원가입</nuxt-link>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="isLoginGetters">
+            <button type="button" class="btn btn-primary">
+              <nuxt-link to="/authentication/mypage">마이페이지</nuxt-link>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="isLoginGetters">
+            <button id="logout" type="button" class="btn btn-primary">
+              <a @click="logout">로그아웃</a>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="isManagerGetters"
+            ><button type="button" class="btn btn-primary">
+              <nuxt-link to="/manager/manager">관리자페이지</nuxt-link>
+            </button></b-nav-item
+          >
+          <b-nav-item
+            v-if="isLoginGetters && !isManagerGetters && !isDoctorGetters"
+          >
+            <button type="button" class="btn btn-primary">
+              <nuxt-link to="/reserve/applyReservation">예약신청</nuxt-link>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="isLoginGetters && !isManagerGetters">
+            <button type="button" class="btn btn-primary">
+              <nuxt-link to="/reserve/checkReservation">예약확인</nuxt-link>
+            </button>
+          </b-nav-item>
+          <b-nav-item v-if="isLoginGetters">
+            <button type="button" class="btn btn-primary">
+              <h5 style="float: left">
+                안녕하세요! {{ this.$store.state.login.login.userName }}
+                <span style="font-size: 14px" v-if="isDoctorGetters">의사</span
+                >님
+                <img
+                  v-if="isLoginGetters"
+                  src="@/assets/images/noProfile.png"
+                  alt=""
+                  width="24"
+                  height="20"
+                  style="
+                    border-radius: 50%;
+                    margin-left: 10px;
+                    padding-bottom: 3px;
+                  "
+                  class="d-inline-block align-text-top"
+                />
+              </h5>
+            </button>
+          </b-nav-item>
+        </b-navbar-nav>
+        <!--  -->
       </b-collapse>
     </client-only>
   </div>
 </template>
 
 <style scoped>
-@media (min-width: 1200px) {
+/* @media (min-width: 1200px) {
   #mystate {
-    left: 200px;
+    left: 250px;
   }
+} */
+
+#right-nav {
+  padding-left: 200px;
+  padding-right: 10px;
 }
 </style>
 
 <script>
-
-export default ({
-    computed: {
-        isLoginGetters() {
-            return this.$store.getters["login/isLogin"];
-        },
-        isManagerGetters() {
-            return this.$store.getters["login/isManager"];
-        },
-        isDoctorGetters() {
-            return this.$store.getters["login/isDoctor"];
-        }
+export default {
+  computed: {
+    isLoginGetters() {
+      return this.$store.getters["login/isLogin"];
     },
-    methods: {
+    isManagerGetters() {
+      return this.$store.getters["login/isManager"];
+    },
+    isDoctorGetters() {
+      return this.$store.getters["login/isDoctor"];
+    },
+  },
+  methods: {
     logout: function () {
       //            localStorage.removeItem('login')
       this.$store.state.login.isLogin = false;
@@ -187,6 +238,6 @@ export default ({
       this.$alertify.success("로그아웃되었습니다. 감사합니다!");
       this.$nuxt.$options.router.push("/authentication/login");
     },
-  }
-})
+  },
+};
 </script>
